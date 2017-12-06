eefa05fe3396f2de95f0bca027ecf258afcd1a8a
Casey Dailey <casey@feasttogether.org>
2017-12-05 20:31:38 -0600

    listing heroes and selecting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
+// Component decorator allows you to mark a class 
+// as an Angular component and provide 
+// additional metadata that determines 
+// how the component should be processed, 
+// instantiated and used at runtime.
+// for more info on the component decorator and component metadata,
+// see documentation here: https://angular.io/api/core/Component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  
+  // handler for the click event on each hero's <li> element
+  // takes the hero object on click and 
+  // sets the value of the instance variable selectedHero
+  // the passed hero object. returns nothing
+  onSelect(hero: Hero): void {
+    
+    this.selectedHero = hero;
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
