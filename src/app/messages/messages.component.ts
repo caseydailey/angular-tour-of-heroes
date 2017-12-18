d75021d41a9cb4ebefd8756edadfb405f52c8017
Casey Dailey <casey@feasttogether.org>
2017-12-18 16:46:29 -0600

    added /heroes route

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,7 +8,7 @@ import { MessageService } from '../message.service';
 })
 export class MessagesComponent implements OnInit {
 
-  // the message service property must be public because you're about to bind it in the template
+  // the message service property must be public
   // angular only binds to public component properties
   constructor(public messageService: MessageService) { }
 
